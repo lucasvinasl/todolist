5b2f1ce82f9112cb21ef75c89528169ce83c2c2a
Lucas Vinícius <lucasvinicius.ctec@gmail.com>
2025-02-20 21:31:32 -0300

    entidade

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,7 +1,17 @@
 package model;
 
 
-import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.scheduling.config.Task;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 
 // Informar que é um entidade - O Hibernate irá gerenciar o CRUD
 @Entity
@@ -10,6 +20,10 @@ import jakarta.persistence.*;
 @Table(name=User.TABLE_NAME)
 
 public class User {
+
+    public interface createUser{}
+
+    public interface attUser{}
 
     // Definir o nome da tabela de usuários
     public static final String TABLE_NAME = "userTb";
@@ -26,9 +40,67 @@ public class User {
     @Column(name = "id", unique = true)
     private Long id;
 
-    @Column(name = "username", length = 255, nullable = false, unique = true)
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    @NotNull(groups = createUser.class)
+    @NotEmpty(groups = createUser.class)
+    @Size(groups = createUser.class, min = 2, max = 100)
     private String username;
 
+
+    @JsonProperty(access =  JsonProperty.Access.WRITE_ONLY)
     @Column(name ="password", length = 100, nullable = false)
+    @NotNull(groups = {createUser.class, attUser.class})
+    @NotEmpty(groups = {createUser.class, attUser.class})
+    @Size(groups = {createUser.class, attUser.class}, min = 8, max = 100)
     private String password;
+
+
+    private List<Task> tasks = new ArrayList<>();
+
+
+    public User(){
+
+    }
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) return false;
+        User user = (User) object;
+        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(password, user.password) && Objects.equals(tasks, user.tasks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, tasks);
+    }
 }
